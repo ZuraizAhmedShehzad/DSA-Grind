02a345e30cb3fba43ae2d6663f42620a02be9426
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2023-10-21 15:49:52 +0500

    Time: 33 ms (7.17%), Space: 27.8 MB (6.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cs
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -1,36 +1,21 @@
 public class Solution {
-    public int UniquePaths(int m, int n)
-    {
-        // Create a memoization table to store computed results
-        int[,] memo = new int[m, n];
-        
-        // Helper function for recursive memoization
-        int Helper(int row, int col)
-        {
-            // Base case: if we reach the bottom-right corner, there's only one path
-            if (row == m - 1 && col == n - 1)
-                return 1;
-            
-            // If we've already computed the result, return it from the memo table
-            if (memo[row, col] != 0)
-                return memo[row, col];
-            
-            // Calculate the number of unique paths by moving right and down
-            int right = 0;
-            int down = 0;
-            
-            if (col < n - 1)
-                right = Helper(row, col + 1);
-            
-            if (row < m - 1)
-                down = Helper(row + 1, col);
-            
-            // Store the result in the memo table and return it
-            memo[row, col] = right + down;
-            return memo[row, col];
+    Dictionary<(int,int),int> dp = new Dictionary<(int,int),int>();
+    public int UniquePaths(int m, int n) {
+        return Dp(m, n, 0, 0);
+    }
+
+    public int Dp(int m, int n, int currm, int currn){
+        if(currm >= m || currn >= n){
+            return 0;
+        }
+        if(currm == m-1 && currn == n-1){
+            return 1;
+        }
+
+        if(!dp.ContainsKey((currm,currn))){
+            dp[(currm,currn)] = Dp(m,n,currm+1,currn) + Dp(m,n,currm,currn+1);
         }
         
-        // Start the recursion from the top-left corner
-        return Helper(0, 0);
+        return dp[(currm,currn)];
     }
 }
