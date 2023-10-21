aa9bf54a00efb79177487d39eda9bbc57d006060
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2023-10-21 18:01:00 +0500

    Time: 26 ms (25.19%), Space: 26.7 MB (62.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cs
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -1,21 +1,19 @@
 public class Solution {
-    Dictionary<(int,int),int> dp = new Dictionary<(int,int),int>();
     public int UniquePaths(int m, int n) {
-        return Dp(m, n, 0, 0);
-    }
-
-    public int Dp(int m, int n, int currm, int currn){
-        if(currm >= m || currn >= n){
-            return 0;
+        int[,] dp = new int[m,n];
+        for(int i=0;i<m;i++){
+            dp[i,0] = 1;
         }
-        if(currm == m-1 && currn == n-1){
-            return 1;
+        for(int i=0;i<n;i++){
+            dp[0,i] = 1;
         }
 
-        if(!dp.ContainsKey((currm,currn))){
-            dp[(currm,currn)] = Dp(m,n,currm+1,currn) + Dp(m,n,currm,currn+1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i,j] = dp[i-1,j] + dp[i,j-1];
+            }
         }
-        
-        return dp[(currm,currn)];
+
+        return dp[m-1,n-1];
     }
 }
