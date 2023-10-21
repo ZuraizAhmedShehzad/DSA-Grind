0729e20044242587986c74fe5d0e04bd92744bf1
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2023-10-22 04:08:37 +0500

    Time: 28 ms (42.44%), Space: 27 MB (8.95%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cs
+++ 1013-fibonacci-number/1013-fibonacci-number.cs
@@ -1,17 +1,36 @@
 public class Solution {
-    public int Fib(int n) {
-        int[] dp = new int[3];
-        dp[0] = 0;
+    //Tabulation
+    // public int Fib(int n) {
+    //     int[] dp = new int[3];
+    //     dp[0] = 0;
         
-        if(n>0)
-         dp[1] = 1; 
+    //     if(n>0)
+    //      dp[1] = 1; 
 
-        for(int i=2;i<=n;i++){
-            dp[2] = dp[1]+dp[0];
-            dp[0] = dp[1];
-            dp[1] = dp[2];
-        }
+    //     for(int i=2;i<=n;i++){
+    //         dp[2] = dp[1]+dp[0];
+    //         dp[0] = dp[1];
+    //         dp[1] = dp[2];
+    //     }
 
-        return dp[1];
+    //     return dp[1];
+    // }
+    
+    //Memoization
+    Dictionary<int,int> dp = new Dictionary<int,int>();
+    public int Fib(int n) {
+        return Dp(n,dp);
+    }
+    public int Dp(int n, Dictionary<int,int> dp){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        
+        if(dp.ContainsKey(n))
+            return dp[n];
+        
+        dp[n] = Dp(n-1,dp)+Dp(n-2,dp);
+        return dp[n];
     }
 }
