a1c5371a16bf858270851769657b535fdcd95a17
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2023-10-21 14:58:49 +0500

    Time: 25 ms (54.2%), Space: 26.4 MB (93.04%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cs
+++ 1013-fibonacci-number/1013-fibonacci-number.cs
@@ -1,23 +1,17 @@
 public class Solution {
-    // public int Fib(int n) {
-    //     if(n == 0)
-    //         return 0;
-    //     if(n == 1)
-    //         return 1;
-
-    //     return Fib(n-1)+Fib(n-2);
-    // }
     public int Fib(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int[3];
         dp[0] = 0;
         
         if(n>0)
          dp[1] = 1; 
 
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            dp[2] = dp[1]+dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
 
-        return dp[n];
+        return dp[1];
     }
 }
