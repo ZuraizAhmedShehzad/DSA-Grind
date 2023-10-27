7e475cdad400202e60871fd94c088603f2b8772a
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2023-10-27 16:03:25 +0500

    Time: 94 ms (28.19%), Space: 49.5 MB (58.33%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode tail = result;
+
+        int carry = 0;
+        
+        while(l1 != null || l2 != null){
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum/10;
+            int reminder = sum % 10;
+            tail.next = new ListNode(reminder);
+            tail = tail.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        if(carry > 0)
+            tail.next = new ListNode(carry);
+
+        return result.next;
+    }
+
+}
